b0630d246312ce2bd21ae72eed7b03c0f0b4674b
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2024-11-03 10:39:20 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ const projectrouter = require("./Router/ProjectRouter")
 //MiddleWare
 app.use(express.json())
 app.use(cors({
-    origin:"https://portfolio-generator-frontend-xi.vercel.app",
+    origin:"http://localhost:3000",
     credentials:true
 }))
 app.use(cookieParser())
